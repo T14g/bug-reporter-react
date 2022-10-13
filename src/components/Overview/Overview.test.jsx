651dd1a6fcca23776f683993e50f3c61c563d9d9
Tiago <ottiagos@gmail.com>
2022-10-12 21:37:34 -0300

    Overview component test passed

--- src/components/Overview/Overview.test.jsx
+++ src/components/Overview/Overview.test.jsx
@@ -1,9 +1,11 @@
 import '@testing-library/react';
 import { render } from '@testing-library/react';
 
+import Overview from './index';
+
 describe('Overview component tests' ,() =>{
     it('should be in the document', ()=>{
         const view = render(<Overview />);
-        expect(view).toBeInTheDocument();
+        expect(view.baseElement).toBeInTheDocument();
     });
 });
---
+++ src/components/Overview/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Overview = () => <div>Overview component</div>;
+
+export default Overview;
